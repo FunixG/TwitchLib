858d7e883284b2dd88f173e59047bcdc9a1134dd
Antoine <funixgaming7@gmail.com>
2021-09-07 20:19:20 +0300

    todo added

---
+++ src/main/java/fr/funixgaming/twitch/api/reference/entities/responses/channel/Stream.java
@@ -0,0 +1,5 @@
+package fr.funixgaming.twitch.api.reference.entities.responses.channel;
+
+//todo https://dev.twitch.tv/docs/api/reference#get-streams
+public class Stream {
+}
---
+++ src/main/java/fr/funixgaming/twitch/api/reference/entities/responses/twitch/User.java
@@ -0,0 +1,5 @@
+package fr.funixgaming.twitch.api.reference.entities.responses.twitch;
+
+//todo https://dev.twitch.tv/docs/api/reference#get-users
+public class User {
+}
